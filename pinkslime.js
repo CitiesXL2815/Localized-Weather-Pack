4cca97644232e1aa17aea373e0eb1be645b0a060
Zygarde <bolty2.2815@gmail.com>
2023-06-27 13:58:31 -0700

    Pink Slime

---
+++ pinkslime.js
@@ -0,0 +1,4 @@
+//ServerEvents.recipes(event=> {
+  //event.recipes.thermal.refinery([Item.of('industrialforegoing:pink_slime').withChance(100), Fluid.of('minecraft:water', 50)], Fluid.of('industrialforegoing:pink_slime', 500)).energy(20000)
+
+//})
